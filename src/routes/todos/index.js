812b6ff29e93830e0c7572578b7cee2d3556bef6
David Karolyi <davidkarolyi96@gmail.com>
2020-08-06 18:36:35 +0000

    feat: added get todos route

--- src/routes/todos/index.js
+++ src/routes/todos/index.js
@@ -1,6 +1,25 @@
 const express = require("express");
+const { body, param } = require("express-validator");
+const { validateAuthor } = require("../../middlewares/validateAuthor");
+const {
+  createTodo,
+  getTodosByAuthorID,
+  deleteTodo,
+  updateTodo,
+  todoExistsUnderAuthor,
+} = require("../../db/operations");
+const { validateRequest } = require("../../middlewares/validateRequest");
 
 const todosRouter = express.Router();
+
+todosRouter.get("/", validateAuthor, async (req, res, next) => {
+  try {
+    const todos = await getTodosByAuthorID(req.authorID);
+    res.json({ todos });
+  } catch (error) {
+    next(error);
+  }
+});
 
 
 module.exports = {
